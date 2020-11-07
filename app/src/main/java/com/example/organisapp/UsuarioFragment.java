58942ab1cbb5232dc1fd3b72ad5d474c74d8588b
ReyVega <a01114523@itesm.mx>
2020-11-07 14:28:16 -0600

    Modificar datos de usuario casi listo

--- app/src/main/java/com/example/organisapp/UsuarioFragment.java
+++ app/src/main/java/com/example/organisapp/UsuarioFragment.java
@@ -4,12 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.organisapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class UsuarioFragment extends Fragment {
 
@@ -17,5 +24,42 @@ public class UsuarioFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_usuario, container, false);
         return root;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle state) {
+        super.onActivityCreated(state);
+
+        EditText nombre2 = (EditText) getView().findViewById(R.id.nombreModificar);
+        EditText apellidos2 = (EditText) getView().findViewById(R.id.apellidosModificar);
+
+        nombre2.setText(MainActivity.usuarioActual.getNombre());
+        apellidos2.setText(MainActivity.usuarioActual.getApellidos());
+
+        Button botonM = (Button) getView().findViewById(R.id.botonModificar);
+
+        botonM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText nombre = (EditText) getView().findViewById(R.id.nombreModificar);
+                EditText apellidos = (EditText) getView().findViewById(R.id.apellidosModificar);
+                EditText password = (EditText) getView().findViewById(R.id.passwordModificar);
+                EditText confirmacion = (EditText) getView().findViewById(R.id.confirmacionModificar);
+
+                if(!(nombre.getText().toString().trim().equals("") ||
+                        apellidos.getText().toString().trim().equals("") ||
+                        password.getText().toString().trim().equals("") ||
+                        confirmacion.getText().toString().trim().equals(""))) {
+                    if(password.getText().toString().equals(confirmacion.getText().toString())) {
+
+                    } else {
+                        Toast.makeText(getActivity().getBaseContext(), "La contraseña no coincide", Toast.LENGTH_SHORT);
+                    }
+                } else {
+                    Toast.makeText(getActivity().getBaseContext(), "Rellenar campos faltantes", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+
     }
 }
