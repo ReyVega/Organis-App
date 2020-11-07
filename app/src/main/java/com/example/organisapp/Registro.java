e7d32cf9b467e982f321957b16e07c082d19c0a0
ReyVega <a01114523@itesm.mx>
2020-11-07 12:17:32 -0600

    Implementación de registro

--- app/src/main/java/com/example/organisapp/Registro.java
+++ app/src/main/java/com/example/organisapp/Registro.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,7 +21,25 @@ public class Registro extends AppCompatActivity {
         buttonOrganis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openOrganis();
+                EditText nombre = (EditText) findViewById(R.id.nombreRegistro);
+                EditText apellidos = (EditText) findViewById(R.id.apellidosRegistro);
+                EditText correo = (EditText) findViewById(R.id.correoRegistro);
+                EditText password = (EditText) findViewById(R.id.passwordRegistro);
+                EditText confirmacion = (EditText) findViewById(R.id.confirmacionRegistro);
+
+                if(!(nombre.getText().toString().trim().equals("") ||
+                        apellidos.getText().toString().trim().equals("") ||
+                        correo.getText().toString().trim().equals("") ||
+                        password.getText().toString().trim().equals("") ||
+                        confirmacion.getText().toString().trim().equals(""))) {
+                    if(password.getText().toString().equals(confirmacion.getText().toString())) {
+                        openOrganis();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Confirmación de contraseña inválida", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Rellenar todos los campos", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
